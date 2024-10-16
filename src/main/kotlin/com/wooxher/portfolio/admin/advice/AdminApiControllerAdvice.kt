24716c4257f18d78fd8a2bdaf93ae9c28c14a2b8
Woocheol <127946067+WooXher@users.noreply.github.com>
2024-10-16 22:13:14 +0900

    공통개발 - Exception과 Advice

--- src/main/kotlin/com/wooxher/portfolio/admin/advice/AdminApiControllerAdvice.kt
+++ src/main/kotlin/com/wooxher/portfolio/admin/advice/AdminApiControllerAdvice.kt
@@ -1,8 +1,39 @@
 package com.wooxher.portfolio.admin.advice
 
+import com.wooxher.portfolio.admin.exception.AdminException
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+// 다른 컨트롤러에서 예외가 발생하는것을 처리하는 핸들러들ㅇ르 모으는 클래스에 적용하는 어노테이션
 @RestControllerAdvice
 class AdminApiControllerAdvice {
 
+    val log = LoggerFactory.getLogger(AdminApiControllerAdvice::class.java)
+
+    @ExceptionHandler
+    fun handleException(e: AdminException): ResponseEntity<String>{
+        log.info(e.message, e)
+
+        return ResponseEntity.status(e.httpStatus).body(e.message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: MethodArgumentNotValidException): ResponseEntity<String>{
+        log.info(e.message, e)
+
+        val fieldError = e.bindingResult.fieldErrors[0]
+        val message = "[${fieldError.field} ${fieldError.defaultMessage}]"
+
+        return ResponseEntity.badRequest().body(message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: Exception): ResponseEntity<String>{
+        log.info(e.message, e)
+
+        return ResponseEntity.internalServerError().body("시스템 오류가 발생했습니다.")
+    }
 }
--- src/main/kotlin/com/wooxher/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/wooxher/portfolio/admin/exception/AdminException.kt
@@ -6,7 +6,19 @@ abstract class AdminException(
     httpStatus: HttpStatus,
     message: String,
 ) :RuntimeException(message){
-
     val httpStatus: HttpStatus = httpStatus
+}
+
+class AdminBadRequestException(message: String):AdminException(
+    httpStatus = HttpStatus.BAD_REQUEST,
+    message = message,
+){
+
+}
+
+class AdminInternalServerErrorException(message: String): AdminException(
+    httpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+    message = message,
+){
 
 }
