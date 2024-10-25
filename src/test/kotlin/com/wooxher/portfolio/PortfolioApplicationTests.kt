0857610936dcfb168a2f519487f05f3d9708c7e8
Woocheol <127946067+WooXher@users.noreply.github.com>
2024-10-26 00:25:39 +0900

    도커로 mysql 실행하기 - 실행안됨

--- src/test/kotlin/com/wooxher/portfolio/PortfolioApplicationTests.kt
+++ src/test/kotlin/com/wooxher/portfolio/PortfolioApplicationTests.kt
@@ -1,5 +1,7 @@
 package com.wooxher.portfolio
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -15,5 +17,25 @@ class PortfolioApplicationTests {
 	fun test(){
 		val encrypted = BCryptPasswordEncoder().encode("비밀번호입니다.")
 		println(encrypted)
+
+	}
+	@Test
+	fun jasypt() {
+		val plainText = "wjddncjf"
+		val encryptor = PooledPBEStringEncryptor()
+		val config = SimpleStringPBEConfig()
+		config.password = "q1w2e3"
+		config.algorithm = "PBEWithMD5AndDES"
+		config.setKeyObtentionIterations("1000")
+		config.setPoolSize("1")
+		config.providerName = "SunJCE"
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator")
+		config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator")
+		config.stringOutputType = "base64"
+		encryptor.setConfig(config)
+		val encryptedText: String = encryptor.encrypt(plainText)
+		val decryptedText: String = encryptor.decrypt(encryptedText)
+		println(encryptedText)e
+		println(decryptedText)
 	}
 }
